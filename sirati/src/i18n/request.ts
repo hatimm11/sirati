43eafc8b2c0bd87263a58900967b7ecf60abd578
hatimm11 <hatimiphone13@gmail.com>
2026-06-09 09:51:56 +0300

    Remove unused import of cookies in request.ts

--- sirati/src/i18n/request.ts
+++ sirati/src/i18n/request.ts
@@ -1,5 +1,4 @@
 import { getRequestConfig } from 'next-intl/server';
-import { cookies } from 'next/headers';
 
 export default getRequestConfig(async ({ requestLocale }) => {
   const locale = (await requestLocale) ?? 'ar';
